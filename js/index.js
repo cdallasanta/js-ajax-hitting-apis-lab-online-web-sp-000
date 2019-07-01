2628a27faf2cb24afe3430b73b09e2abde984e2f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 22:09:16 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 function getRepositories(){
   console.log("cool");
   const req = new XMLHttpRequest();
-  const username = document.getElementById("username").innerHTML;
+  const username = document.getElementById("username").value;
   req.addEventListener('load', showRepositories);
   req.open('get', `https://api.github.com/${username}/repos`);
   req.send();
